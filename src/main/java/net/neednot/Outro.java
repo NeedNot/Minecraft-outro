3b717f247598af5c000ea5cc862ac13795ea01a5
NeedNot <daltonhornor0@gmail.com>
2022-10-27 16:34:43 -0700

    you can now play and stop the outro by clicking the laptop

--- src/main/java/net/neednot/Outro.java
+++ src/main/java/net/neednot/Outro.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.neednot.blocks.Laptop;
@@ -16,6 +18,8 @@ public class Outro implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("outro");
 	public static final Block lAPTOP = new Laptop(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+	public static final Identifier OUTRO_MUSIC = new Identifier("outro:outro");
+	public static SoundEvent PLAY_OUTRO = new SoundEvent(OUTRO_MUSIC);
 
 	@Override
 	public void onInitialize() {
@@ -23,7 +27,7 @@ public class Outro implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		Registry.register(Registry.BLOCK, new Identifier("outro", "laptop"), lAPTOP);
-
+		Registry.register(Registry.SOUND_EVENT, Outro.OUTRO_MUSIC, PLAY_OUTRO);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/neednot/blocks/Laptop.java
+++ src/main/java/net/neednot/blocks/Laptop.java
@@ -3,18 +3,29 @@ package net.neednot.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.math.random.RandomSplitter;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.neednot.Outro;
 
 
 public class Laptop extends Block {
@@ -22,6 +33,7 @@ public class Laptop extends Block {
         super(settings);
         setDefaultState(getStateManager().getDefaultState().with(PAUSED, true));
     }
+    SoundInstance soundInstance;
     public static final BooleanProperty PAUSED = BooleanProperty.of("paused");
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
@@ -35,7 +47,61 @@ public class Laptop extends Block {
     }
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
+        if (soundInstance == null) {
+            soundInstance = new PositionedSoundInstance(Outro.PLAY_OUTRO , SoundCategory.AMBIENT , 1 , 1 , new Random() {
+                @Override
+                public Random split() {
+                    return null;
+                }
+
+                @Override
+                public RandomSplitter nextSplitter() {
+                    return null;
+                }
+
+                @Override
+                public void setSeed(long seed) {
+
+                }
+
+                @Override
+                public int nextInt() {
+                    return 0;
+                }
+
+                @Override
+                public int nextInt(int bound) {
+                    return 0;
+                }
+
+                @Override
+                public long nextLong() {
+                    return 0;
+                }
+
+                @Override
+                public boolean nextBoolean() {
+                    return false;
+                }
+
+                @Override
+                public float nextFloat() {
+                    return 0;
+                }
+
+                @Override
+                public double nextDouble() {
+                    return 0;
+                }
+
+                @Override
+                public double nextGaussian() {
+                    return 0;
+                }
+            } , pos);
+        }
+        if (world.getBlockState(pos).get(PAUSED)) MinecraftClient.getInstance().getSoundManager().play(soundInstance);
+        else MinecraftClient.getInstance().getSoundManager().stopAll();
         world.setBlockState(pos, state.with(PAUSED, !world.getBlockState(pos).get(PAUSED)));
         return ActionResult.SUCCESS;
     }
