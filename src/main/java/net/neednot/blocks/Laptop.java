b0e5a627605e697082c1a6a8d178a3526ec28419
NeedNot <daltonhornor0@gmail.com>
2022-10-27 15:18:22 -0700

    laptop shows in game

---
+++ src/main/java/net/neednot/blocks/Laptop.java
@@ -0,0 +1,20 @@
+package net.neednot.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+
+public class Laptop extends Block {
+    public Laptop(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.2f, 1f);
+    }
+}
