9fcfc4ce9fca0603c889a8b1e87d4a0832120d1b
NeedNot <daltonhornor0@gmail.com>
2022-10-27 15:34:27 -0700

    laptop now has block states

--- src/main/java/net/neednot/blocks/Laptop.java
+++ src/main/java/net/neednot/blocks/Laptop.java
@@ -3,18 +3,40 @@ package net.neednot.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 
 public class Laptop extends Block {
     public Laptop(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(PAUSED, true));
     }
+    public static final BooleanProperty PAUSED = BooleanProperty.of("paused");
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.2f, 1f);
+    }
+    //right click the laptop to toggle paused and play states
+    //play state goes to paused ater it's over
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(PAUSED);
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
+        world.setBlockState(pos, state.with(PAUSED, !world.getBlockState(pos).get(PAUSED)));
+        return ActionResult.SUCCESS;
     }
 }
